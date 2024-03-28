be494a33c377d60aea411bdf07e29b35592606cf
zsc000722 <1607942643@qq.com>
2024-03-28 11:31:14 +0800

    finetune

---
+++ finetune.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=8 python main.py --config cfgs/PointGPT-S/finetune_scan_hardest.yaml \
+--finetune_model --exp_name test --ckpts /home/zsc/PointGPT/ckpt/PointGPT-S/pretrained.pth
